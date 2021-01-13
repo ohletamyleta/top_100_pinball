9a6969754fd96bc2a07d92a1e9b2e9ebb74f3b18
ohletamyleta <ohletamyleta@gmail.com>
2021-01-13 16:45:52 -0500

    renamed file

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ require 'open-uri'
 
 require_relative '../lib/cli'
 require_relative '../lib/scraper'
-require_relative '../lib/Top100'
+require_relative '../lib/Top50'
+require_relative '../lib/Info'
 require_relative '../lib/version'
